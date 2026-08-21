6ff7e6e508a9e326a481c6f954bad9a7137db206
bigteach85 <kenneth.george.jr@gmail.com>
2026-08-21 12:01:49 -0400

    Route public Our Story page

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import OurStory from './OurStory.jsx'
 import CommunityCounter from './CommunityCounter.jsx'
+
+const isOurStory = window.location.pathname === '/our-story' || window.location.pathname === '/our-story/'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-    <CommunityCounter />
+    {isOurStory ? <OurStory /> : <><App /><CommunityCounter /></>}
   </StrictMode>,
 )
